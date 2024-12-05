7b1132956aa41fac16f9e45c329586f8ce019f93
MarchoSaurio <112833842+MarchoSaurio@users.noreply.github.com>
2024-12-04 22:17:00 -0600

    Update login_proces.php

--- ProyectoLibreria/phpUsuario/login_proces.php
+++ ProyectoLibreria/phpUsuario/login_proces.php
@@ -8,28 +8,33 @@ $dbConn = connect($db);
 // Obtener datos del formulario
 $variable = $_POST['username'];
 $password = $_POST['password'];
+
 // Buscar el usuario en la base de datos
 $Query = "SELECT * FROM usuarios WHERE nombre_us =:nombre_us OR email=:email";
 $statement = $dbConn->prepare($Query);
-$statement->bindValue(':nombre_us',$_POST['username']);
-$statement->bindValue(':email',$_POST['username']);
+$statement->bindValue(':nombre_us', $_POST['username']);
+$statement->bindValue(':email', $_POST['username']);
 $statement->execute();
+
 if ($statement === false) {
     die("Error al ejecutar la consulta: " . $mysql->error);
 }
-$array=$statement->fetch(PDO::FETCH_ASSOC);
-if ($statement->rowCount()> 0 ) {
-    if (($variable==$array['email'] && $password==$array['password'])||($variable==$array['nombre_us'] && $password==$array['password'])){ 
-        echo '<script> alert("Puedes entrar");</script>';
-        }else{ 
-        echo '<script> alert("Nombre de usuario ocupado");location.replace("../html/login.html"); </script>';
-       }
-        exit; // Finalizar el script después de redirigir   
+
+$array = $statement->fetch(PDO::FETCH_ASSOC);
+if ($statement->rowCount() > 0) {
+    if (($variable == $array['email'] && $password == $array['password']) || ($variable == $array['nombre_us'] && $password == $array['password'])) {
+        if ($array['administrador'] == 1) {
+            header("Location: ../admin/usuarios.html");
+        } else {
+            header("Location: ../html/inicioUsuario.html");
+        }
+        exit;
+    } else { 
+        echo '<script>Alert("Nombre de usuario o contraseña incorrectos.");</script>';
+    }
+    exit;
 } else {
-    echo '<script> alert("No existe el usuario");location.replace("login.html"); </script>';
-    // Si el nombre de usuario no esta usado lo registra  
-    exit; // Finalizar el script después de redirigir
+    echo '<script>Alert("No existe el usuario");</script>';
+    exit; 
 }
-// Cerrar la conexión
-$mysql->close();
 ?>
